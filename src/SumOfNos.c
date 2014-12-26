9be0f5c6685c31b21ff9e0a4f318580c786708fd
magec101 <magec101@manipalglobal.com>
2014-12-26 09:52:07 +0000

    Submit from IDE @Fri Dec 26 15:22:07 Asia/Kolkata 2014

--- src/SumOfNos.c
+++ src/SumOfNos.c
@@ -4,74 +4,14 @@
 
 int sumOfNosMain(int argc, const char** argv);
 
-int sum_of_digits (int n);
-int sum_of_sq_digits (int n);
-int sum_of_cube_digits (int n);
-
 
 int sumOfNosMain(int argc, const char** argv) {
 		
-		int iOperation;
-		int iNos;
-		
-		if (argc < 2) 
-		{
-    		scanf("%d", &iOperation);
-    		scanf("%d", &iNos);
-	    }
-    	else
-    	{
-			iOperation = atoi(argv[1]);
-			iNos = atoi(argv[2]);
-    	}
-
-        switch(iOperation)
-        {
-        	case 1:
-        	return (sum_of_digits(iNos));
-        	break;
-        	
-        	case 2:
-        	return (sum_of_sq_digits(iNos));
-        	break;
-        	
-        	case 3:
-        	return (sum_of_cube_digits(iNos));
-        	break;
-        	
-         }
+	/* Write your code here */
 
 }
 
-int sum_of_digits (int n)
-{
-	int i;
-	int iSum = 0;
-	
-	for (i =1; i<=n; i++)
-		iSum = iSum +i;
-	return iSum;
-}
 
-int sum_of_sq_digits (int n)
-{
-	int i;
-	int iSum = 0;
-	
-	for (i =1; i<=n; i++)
-		iSum = iSum + (i*i);
-	return iSum;
-}
-
-int sum_of_cube_digits (int n)
-{
-	int i;
-	int iSum = 0;
-	
-	for (i =1; i<=n; i++)
-		iSum = iSum + (i*i*i);
-	return iSum;
-}
 
 #ifndef __CXX_TEST_MAIN__
 
